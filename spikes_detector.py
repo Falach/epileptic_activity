8d340d28855a3e5215c1874576dbf3c25e49728b
Falach <rotemfa0@gmail.com>
2021-03-31 21:04:49 +0300

    save spikes data in df

--- spikes_detector.py
+++ spikes_detector.py
@@ -1,11 +1,16 @@
 import mne
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import math
 from scipy import stats, signal
+import itertools
+import pandas as pd
+mpl.use('Agg')
+
 
 # general constants
-# TODO: check how to handle 128 Hz
+# TODO: check how to handle 128 Hz (filter is making problems)
 sampling_rate = 1000
 min_distance = 200  # minimal distance for 'different' spikes - in miliseconds
 
@@ -34,35 +39,46 @@ min_spike_length_ms = 5  # a spike is detected if there are points for X ms pass
 conditions_true_if_any = False
 percentageOfNansAllowedAroundSpike = 0.1  # how many NaNs are allowed in the vicinity of the spike(vicinity=minDistSpikes / 2 before and after)
 HighBandPassScore = 11  # this is for debugging - finding especially high STDs for HP
+common_spikes_index = []
 
 
-def get_markers(curr_block, points_above_thresh, z_score):
+# format: [spike_id, threshold_type, first_index, last_index, max_index, max_amp, duration(calc once for all after convert to df), sleep_stage?]
+spikes_list = []
+id_count = itertools.count(1)
+
+
+def get_markers(data, index_above_threshold, thresh_type):
     max_markers_index = []
     max_marker_value = []
 
-    # get indexes from z_score values
-    if len(points_above_thresh) > 0:
-        index_above_threshold = (z_score > threshold_amp).nonzero()[0]
-
-        # find max markers
-        counter = 1
-        for j in range(len(index_above_threshold)):
-            # check that the next index is the same spike
-            if j + 1 < len(index_above_threshold) and index_above_threshold[j + 1] - index_above_threshold[j] == 1:
-                counter += 1
-            # the current spike finished
+    # find max markers
+    counter = 1
+    curr_spike = [next(id_count), thresh_type, index_above_threshold[0]]
+    for j in range(len(index_above_threshold)):
+        # check that the next index is the same spike
+        if j + 1 < len(index_above_threshold) and index_above_threshold[j + 1] - index_above_threshold[j] == 1:
+            counter += 1
+        # the current spike finished
+        else:
+            if counter == 1:
+                value = data[index_above_threshold[j]]
+                index = index_above_threshold[j]
+                max_marker_value.append(value)
+                max_markers_index.append(index)
             else:
-                if counter == 1:
-                    max_marker_value.append(curr_block[index_above_threshold[j]])
-                    max_markers_index.append(index_above_threshold[j])
-                else:
-                    # check if the peak is positive or negative and append it's value
-                    max_value = curr_block[index_above_threshold[j - counter + 1]: index_above_threshold[j] + 1].max()
-                    min_value = curr_block[index_above_threshold[j - counter + 1]: index_above_threshold[j] + 1].min()
-                    value = max_value if abs(max_value) > abs(min_value) else min_value
-                    max_marker_value.append(value)
-                    max_markers_index.append(np.intersect1d(np.where(curr_block == value)[0], index_above_threshold[j - counter + 1: j + 1])[0])
-                    counter = 1
+                # check if the peak is positive or negative and append it's value
+                max_value = data[index_above_threshold[j - counter + 1]: index_above_threshold[j] + 1].max()
+                min_value = data[index_above_threshold[j - counter + 1]: index_above_threshold[j] + 1].min()
+                value = max_value if abs(max_value) > abs(min_value) else min_value
+                index = np.intersect1d(np.where(data == value)[0], index_above_threshold[j - counter + 1: j + 1])[0]
+                max_marker_value.append(value)
+                max_markers_index.append(index)
+                counter = 1
+
+            curr_spike.extend((index_above_threshold[j], index, value))
+            spikes_list.append(curr_spike)
+            if j + 1 < len(index_above_threshold):
+                curr_spike = [next(id_count), thresh_type, index_above_threshold[j + 1]]
 
     return np.array(max_markers_index), np.array(max_marker_value)
 
@@ -80,12 +96,19 @@ def detect(data):
         if use_amp or use_amp_env or use_amp_grad:
             z_score_amp = stats.zscore(curr_block)
             points_above_thresh_amp = z_score_amp[z_score_amp > threshold_amp]
+            # get indexes from z_score values and add offset of the current block
+            if len(points_above_thresh_amp) > 0:
+                index_above_threshold_amp = (z_score_amp > threshold_amp).nonzero()[0] + i * points_in_block
+                max_markers_index_amp, max_marker_value_amp = get_markers(data, index_above_threshold_amp, 'amp')
 
         # check gradient threshold
         if use_grad or use_amp_grad:
             gradient_diff = np.diff(curr_block)
             z_score_grad = stats.zscore(np.insert(gradient_diff, 0, 0))
             points_above_thresh_grad = z_score_grad[z_score_grad > threshold_grad]
+            if len(points_above_thresh_grad) > 0:
+                index_above_threshold_grad = (z_score_grad > threshold_grad).nonzero()[0] + i * points_in_block
+                max_markers_index_grad, max_marker_value_grad = get_markers(data, index_above_threshold_grad, 'grad')
 
         # check envelope threshold
         if use_env or use_amp_env:
@@ -93,20 +116,14 @@ def detect(data):
             env_block = abs(signal.hilbert(filtered_block))
             z_score_env = stats.zscore(env_block)
             points_above_thresh_env = z_score_env[z_score_env > threshold_env]
-
-        # combine thresholds and get spikes points
-        max_markers_index_amp, max_marker_value_amp = get_markers(curr_block, points_above_thresh_amp, z_score_amp)
-        max_markers_index_grad, max_marker_value_grad = get_markers(curr_block, points_above_thresh_grad, z_score_grad)
-        max_markers_index_env, max_marker_value_env = get_markers(curr_block, points_above_thresh_env, z_score_env)
-
-        # set indexes to the total data
-        max_markers_index_amp = np.array(max_markers_index_amp) + i * points_in_block
-        max_markers_index_grad = np.array(max_markers_index_grad) + i * points_in_block
-        max_markers_index_env = np.array(max_markers_index_env) + i * points_in_block
+            if len(points_above_thresh_env) > 0:
+                index_above_threshold_env = (z_score_env > threshold_env).nonzero()[0] + i * points_in_block
+                max_markers_index_env, max_marker_value_env = get_markers(data, index_above_threshold_env, 'env')
 
         # find the points that are shared in all thresholds
         common_index = np.intersect1d(max_markers_index_amp, max_markers_index_grad)
         all_common_index = np.intersect1d(common_index, max_markers_index_env)
+        common_spikes_index.extend(all_common_index)
         all_common_value = data[all_common_index] if len(all_common_index) > 0 else []
         plt.scatter(all_common_index, all_common_value, marker='D', color='black')
 
@@ -132,9 +149,19 @@ raw = mne.io.read_raw_edf(
 raw.pick_channels(['RAH1M'])
 raw.crop(tmin=1000, tmax=7000)
 
+# format: spike_id: {first_index: 1, last_index: 5, max_amp: 400,, duration:? , sleep_stage: ?}
+# spikes_data = {}
+
+
 # viz.plot_raw(raw, duration=30, scalings=dict(eeg=35))
 # plt.plot(raw.get_data()[0])
 
 data = raw.get_data()[0]
 detect(data)
+spikes_df = pd.DataFrame(spikes_list, columns=['id', 'threshold_type', 'first_index', 'last_index', 'max_index', 'max_amp'])
+# TODO: convert to milliseconds (how? from the beginning of the gradient? only this z-score points above thresh?
+spikes_df['duration'] = spikes_df['last_index'] - spikes_df['first_index']
+spikes_df = spikes_df.astype({"max_index": int})
+spikes_df = spikes_df.loc[spikes_df['max_index'].isin(common_spikes_index)]
+print('check spike list')
 
