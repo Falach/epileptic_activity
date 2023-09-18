fix conversion of channel names from matlab to edf, expand single units analysis

single_units.py
@@ -2,13 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from scipy.interpolate import make_interp_spline
+import glob
 
-def plot_units():
-    subj = '017'
-    chan = 'RA8'
-    cluster = 1
+
+def plot_units(subj='018', chan='RMH7', cluster=1, peaks_file=None):
+    if peaks_file is None:
+        peaks_file = chan[:-1]
     peaks = np.load(
-        fr"C:\repos\NirsLabProject\NirsLabProject\data\products\p{subj}_MTL\bipolar_model\spikes\peaks-{chan[:-1]}1.npz.npy").flatten()
+        fr"C:\repos\NirsLabProject\NirsLabProject\data\products\p{subj}_MTL\bipolar_model\spikes\peaks-{peaks_file}1.npz.npy").flatten()
     spikes = pd.read_csv(fr"D:\Hanna\D{subj}\units\{chan}_unit{cluster}.csv", header=None)[0].to_list()
 
     unit = np.zeros(spikes[-1])
@@ -17,20 +18,25 @@ def plot_units():
     trials_hist = np.zeros(shape=(len(peaks), 1000))
     for i, peak in enumerate(peaks):
         curr = np.array(unit[peak - 500: peak + 500])
-        all_trials[i] = curr
-        curr_hist = np.arange(1000)
-        curr_hist[curr == 0] = 0
-        trials_hist[i] = curr_hist
+        if len(curr) > 0:
+            all_trials[i] = curr
+            curr_hist = np.arange(1000)
+            curr_hist[curr == 0] = 0
+            trials_hist[i] = curr_hist
+        else:
+            all_trials = np.delete(all_trials, np.s_[i:], axis=0)
+            trials_hist = np.delete(trials_hist, np.s_[i:], axis=0)
+            break
+
 
     fig, ax = plt.subplots(2, 1, figsize=[10,5])
 
     # Loop to plot raster for each trial
-    y_index = 0
+    line_size = 0.5 if len(all_trials) < 100 else 1
+    line_size = 4 if len(all_trials) > 400 else 1
     for trial in range(len(all_trials)):
         spike_times = [i for i, x in enumerate(all_trials[trial]) if x == 1]
-        # if len(spike_times) > 0:
-        ax[0].vlines(spike_times, trial - 4, trial + 4)
-            # y_index += 1
+        ax[0].vlines(spike_times, trial - line_size, trial + line_size)
 
     ax[0].set_xlim([0, 1000])
     ax[0].set_ylim([0, len(all_trials)])
@@ -41,21 +47,19 @@ def plot_units():
     # specify tick marks and label y axis
     # ax.set_ylabel('Trial Number')
 
-    ax[0].set_title(f'{subj}-{chan}-unit{cluster}')
+    ax[0].set_title(f'{subj}-{chan}-unit{cluster}-{peaks_file} peaks')
     ax[1].set_title('Peri-Stimulus Time Histogram (PSTH)')
     ax[1].set_ylabel('Firing Rate (Hz)')
 
     # Draw the PSTH
     bin_size = 50
     counts, bins = np.histogram(trials_hist[trials_hist != 0], bins=bin_size)
-    ax[1].hist(bins[:-1], bins, weights=counts/(len(all_trials) * bin_size), alpha=0.5)
+    ax[1].hist(bins[:-1], bins, weights=(counts/(len(all_trials) * bin_size)) * 1000, alpha=0.5)
 
     # smoothing
     x = np.arange(0, 1000, int(1000/bin_size))
-    y = counts / (len(all_trials) * bin_size)
+    y = (counts / (len(all_trials) * bin_size)) * 1000
     X_Y_Spline = make_interp_spline(x, y)
-
-    # Returns evenly spaced numbers over a specified interval.
     X_ = np.linspace(x.min(), x.max(), 500)
     Y_ = X_Y_Spline(X_)
     ax[1].plot(X_, Y_, color='black')
@@ -66,8 +70,86 @@ def plot_units():
     ax[0].axvline(500, color='r')
     ax[1].axvline(500, color='r')
     plt.tight_layout()
-    plt.savefig(fr"D:\Hanna\D{subj}\units\{chan}_unit{cluster}.png")
+    file_name = fr"D:\Hanna\D{subj}\units\{chan}_unit{cluster}.png" if peaks_file is None else fr"D:\Hanna\D{subj}\units\{chan}_unit{cluster}_{peaks_file}peaks.png"
+    plt.savefig(file_name)
     plt.show()
     print()
 
-plot_units()
+
+def plot_combine_units(subj, chan_lst):
+    # draw each unit in different color
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink']
+    peaks = np.load(
+        fr"C:\repos\NirsLabProject\NirsLabProject\data\products\p{subj}_MTL\bipolar_model\spikes\peaks-{chan_lst[0][0][:-1]}1.npz.npy").flatten()
+    # all_units = np.zeros(shape=(len(chan_lst) * len(peaks), 1000))
+    fig, ax = plt.subplots(2, 1, figsize=[16, 10])
+
+    for color, (chan, cluster) in enumerate(chan_lst):
+        spikes = pd.read_csv(fr"D:\Hanna\D{subj}\units\{chan}_unit{cluster}.csv", header=None)[0].to_list()
+
+        unit = np.zeros(spikes[-1])
+        unit[spikes[:-1]] = 1
+        all_trials = np.zeros(shape=(len(peaks), 1000))
+        trials_hist = np.zeros(shape=(len(peaks), 1000))
+        for i, peak in enumerate(peaks):
+            curr = np.array(unit[peak - 500: peak + 500])
+            if len(curr) > 0:
+                all_trials[i] = curr
+                curr_hist = np.arange(1000)
+                curr_hist[curr == 0] = 0
+                trials_hist[i] = curr_hist
+            else:
+                all_trials = np.delete(all_trials, np.s_[i:], axis=0)
+                trials_hist = np.delete(trials_hist, np.s_[i:], axis=0)
+                break
+
+        # Loop to plot raster for each trial
+        line_size = 4 if len(all_trials) > 400 else 1
+        for trial in range(len(all_trials)):
+            spike_times = [i for i, x in enumerate(all_trials[trial]) if x == 1]
+            ax[0].vlines(spike_times, trial - line_size, trial + line_size, color=colors[color])
+
+        bin_size = 50
+        counts, bins = np.histogram(trials_hist[trials_hist != 0], bins=bin_size)
+        # ax[1].hist(bins[:-1], bins, weights=counts / (len(all_trials) * bin_size), alpha=0.5)
+
+        # smoothing
+        x = np.arange(0, 1000, int(1000 / bin_size))
+        y = (counts / (len(all_trials) * bin_size)) * 1000
+        X_Y_Spline = make_interp_spline(x, y)
+        X_ = np.linspace(x.min(), x.max(), 500)
+        Y_ = X_Y_Spline(X_)
+        ax[1].plot(X_, Y_, color=colors[color])
+
+    ax[0].set_xlim([0, 1000])
+    ax[0].set_ylim([0, len(all_trials)])
+    ax[1].set_xlim([0, 1000])
+    ax[1].set_xlabel('Time (ms)')
+    ax[0].axvline(500, color='r')
+    ax[1].axvline(500, color='r')
+    ax[0].set_title(f'{subj}-{chan[:-1]}')
+    ax[1].set_title('Peri-Stimulus Time Histogram (PSTH)')
+    ax[1].set_ylabel('Firing Rate (Hz)')
+
+    plt.tight_layout()
+    plt.savefig(fr"D:\Hanna\D{subj}\units\{subj}-{chan[:-1]}.png")
+
+subj = {'017':[('RA3', 1), ('RA4', 1), ('RA8', 1), ('RA8', 2), ('LPHG1', 1), ('LPHG8', 1), ('LPHG8', 2)],
+        '018':[('RA5', 1), ('RA8', 1), ('RMH2', 1), ('RMH4', 1), ('RMH6', 1), ('RMH6', 3), ('RMH7', 1)],
+        '025':[('LA3', 1), ('LA3', 2), ('LA3', 3)]}
+
+# plot_units()
+subj = '018'
+subj_files_list = glob.glob(f'D:\\Hanna\\D{subj}\\units\\*.csv')
+for file in subj_files_list:
+    chan = file.split('\\')[-1].split('_')[0]
+    cluster = file.split('\\')[-1].split('_')[1].split('.')[0][-1]
+    plot_units(subj, chan, cluster, 'RMH')
+
+# plot_units('017', 'LOF7', 1, 'RA')
+# for s in subj:
+# s = '018'
+# for c in subj[s]:
+#     plot_units(s, c[0], c[1])
+
+# plot_combine_units('018', [x for x in subj['018'] if x[0].startswith('RMH')])

utils.py
@@ -9,6 +9,11 @@ import h5py
 import glob
 import scipy.io as sio
 from mff_to_edf import write_edf as rotem_write_edf
+from mnelab.io.writers import write_edf as mnelab_edf
+from scipy.interpolate import make_interp_spline
+from scipy.stats import kde
+import seaborn as sns
+
 
 
 # epilepsy
@@ -441,78 +446,94 @@ def from_mat_to_edf(subj):
     print()
 
 
-def from_mat_to_edf_Hanna(subj='017', scalp='scoring'):
+def from_mat_to_edf_Hanna(subj='017', scalp=None):
     mne_raw = None
     subj_files_list = glob.glob(f'D:\\Hanna\\D{subj}\\Macro\\*')
     sfreq = 1000
+    ch_names = pd.read_csv(f'D:\\Hanna\\D{subj}\\montage.csv')
+    macro_chans = ch_names[ch_names['IS_MACRO'] == 1]
+    scalp_chans = ch_names[ch_names['IS_SCALP'] == 1]['LOCATION'].tolist()
+    exclude_chans = ch_names[ch_names['EXCLUDE'] == 1]['LOCATION'].tolist()
     for i, curr_file in enumerate(subj_files_list):
         try:
             f = h5py.File(curr_file, 'r')
             data = f.get('denoised_data')
             data = np.array(data)
-            ch_names = pd.read_csv(f'D:\\Hanna\\D{subj}\\montage.csv')
-            macro_chans = ch_names[ch_names['IS_MACRO'] == 1]
-            curr_name = macro_chans['LOCATION'][i].replace(' ', '')
-            info = mne.create_info(ch_names=[curr_name], sfreq=sfreq)
-            if mne_raw is None:
-                mne_raw = mne.io.RawArray(data.T, info)
-            else:
-                if data.size > mne_raw.n_times:
-                    data = data[:mne_raw.n_times]
-                mne_raw.add_channels([mne.io.RawArray(data.T, info)])
+            chan_num = int(curr_file.split('_')[1].replace('.mat', ''))
+            curr_name = macro_chans[macro_chans['CHANNEL'] == chan_num]['LOCATION'].tolist()[0]
+             # = macro_chans['LOCATION'][chan_num].replace(' ', '')
+            if curr_name not in exclude_chans:
+                info = mne.create_info(ch_names=[curr_name], sfreq=sfreq)
+                if mne_raw is None:
+                    mne_raw = mne.io.RawArray(data.T, info)
+                else:
+                    if data.size > mne_raw.n_times:
+                        data = data[:mne_raw.n_times]
+                    mne_raw.add_channels([mne.io.RawArray(data.T, info)])
         except Exception as e:
             pass
 
-    if scalp is not None:
-        scalp = mne.io.read_raw_edf(f'D:\\Hanna\\D{subj}\\d{subj}_scalp_250Hz.edf').resample(sfreq)
-        scalp.load_data()
-        scalp_data = scalp.pick_channels(['Signal 0', 'Signal 1', 'Signal 2', 'Signal 3', 'Signal 4']).get_data()
-        mne_raw.load_data()
-        if mne_raw.n_times < scalp_data.shape[1]:
-            mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
-        else:
-            mne_raw.crop(tmax=scalp_data.shape[1] / sfreq - 1)
-            mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
 
-    rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData.edf')
+    mne_raw.reorder_channels(sorted([x for x in mne_raw.ch_names if x not in scalp_chans]) + sorted(scalp_chans))
+    # if scalp is not None:
+    #     scalp = mne.io.read_raw_edf(f'D:\\Hanna\\D{subj}\\d{subj}_scalp_250Hz.edf').resample(sfreq)
+    #     scalp.load_data()
+    #     scalp_data = scalp.pick_channels(['Signal 0', 'Signal 1', 'Signal 2', 'Signal 3', 'Signal 4']).get_data()
+    #     mne_raw.load_data()
+    #     if mne_raw.n_times < scalp_data.shape[1]:
+    #         mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
+    #     else:
+    #         mne_raw.crop(tmax=scalp_data.shape[1] / sfreq - 1)
+    #         mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
+
+    # rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData_fixed.edf')
+    write_edf(f'D:\\Hanna\\D{subj}\\P{subj}_overnightData_fixed_2.edf', mne_raw)
     print()
 
+from_mat_to_edf_Hanna(subj='025')
 
-# from_mat_to_edf_Hanna()
-
+# for subj in ['013', '017', '025']:
+#     from_mat_to_edf_Hanna(subj=subj)
 
 def from_mat_to_edf_Hanna_sio(subj='479'):
     mne_raw = None
     subj_files_list = glob.glob(f'D:\\Hanna\\D{subj}\\Macro\\*')
     sfreq = 1000
+    ch_names = pd.read_csv(f'D:\\Hanna\\D{subj}\\montage.csv')
+    macro_chans = ch_names[ch_names['IS_MACRO'] == 1]
+    scalp_chans = ch_names[ch_names['IS_SCALP'] == 1]['LOCATION'].tolist()
+    exclude_chans = ch_names[ch_names['EXCLUDE'] == 1]['LOCATION'].tolist()
     for i, curr_file in enumerate(subj_files_list):
         try:
             f = sio.loadmat(curr_file)
             data = f['denoised_data']
             data = np.array(data).T
-            ch_names = pd.read_csv(f'D:\\Hanna\\D{subj}\\montage.csv')
-            macro_chans = ch_names[ch_names['IS_MACRO'] == 1]
-            curr_name = macro_chans['LOCATION'][i].replace(' ', '')
-            info = mne.create_info(ch_names=[curr_name], sfreq=sfreq)
-            if mne_raw is None:
-                mne_raw = mne.io.RawArray(data, info)
-            else:
-                if data.size > mne_raw.n_times:
-                    data = data[:mne_raw.n_times]
-                mne_raw.add_channels([mne.io.RawArray(data, info)])
+            chan_num = int(curr_file.split('_')[1].replace('.mat', ''))
+            curr_name = macro_chans[macro_chans['CHANNEL'] == chan_num]['LOCATION'].tolist()[0]
+            if curr_name not in exclude_chans:
+                info = mne.create_info(ch_names=[curr_name], sfreq=sfreq)
+                if mne_raw is None:
+                    mne_raw = mne.io.RawArray(data, info)
+                else:
+                    if data.size > mne_raw.n_times:
+                        data = data[:mne_raw.n_times]
+                    mne_raw.add_channels([mne.io.RawArray(data, info)])
         except Exception as e:
             pass
 
-    mne_raw.load_data()
-    scalp = mne.io.read_raw_edf(f'D:\\Hanna\\D{subj}\\d{subj}_scalp_250Hz.edf').resample(sfreq)
-    scalp.load_data()
-    scalp_data = scalp.pick_channels(['Signal 0', 'Signal 1', 'Signal 3', 'Signal 4']).get_data()
-    mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
-    rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData.edf')
+    # mne_raw.load_data()
+    # scalp = mne.io.read_raw_edf(f'D:\\Hanna\\D{subj}\\d{subj}_scalp_250Hz.edf').resample(sfreq)
+    # scalp.load_data()
+    # scalp_data = scalp.pick_channels(['Signal 0', 'Signal 1', 'Signal 3', 'Signal 4']).get_data()
+    # mne_raw.add_channels([mne.io.RawArray(scalp_data[:, :mne_raw.n_times], scalp.info)], force_update_info=True)
+    mne_raw.reorder_channels(sorted([x for x in mne_raw.ch_names if x not in scalp_chans]) + sorted(scalp_chans))
+    rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData_fixed.edf')
     print()
 
 # from_mat_to_edf_Hanna_sio(subj='018')
-# from_mat_to_edf_Hanna(subj='018')
+# from_mat_to_edf_Hanna_sio(subj='479')
+# from_mat_to_edf_Hanna_sio(subj='489')
+
 
 
 def from_mat_to_LFP_Hanna(subj='017'):
@@ -530,7 +551,8 @@ def from_mat_to_LFP_Hanna(subj='017'):
                 f = h5py.File(curr_file, 'r')
                 data = f.get('denoised_data')
             data = np.array(data)
-            curr_name = micro_chans['LOCATION'][i].replace(' ', '')
+            chan_num = int(curr_file.split('_')[1].replace('.mat', '')) - 1
+            curr_name = micro_chans['LOCATION'][chan_num].replace(' ', '')
             info = mne.create_info(ch_names=[curr_name], sfreq=sfreq)
             if mne_raw is None:
                 mne_raw = mne.io.RawArray(data[:-10].T, info)
@@ -541,9 +563,32 @@ def from_mat_to_LFP_Hanna(subj='017'):
         except Exception as e:
             pass
 
-    rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData_LFP.edf')
+    mne_raw.reorder_channels(sorted([x for x in mne_raw.ch_names]))
+    rotem_write_edf(mne_raw, f'D:\\Hanna\\D{subj}\\P{subj}_overnightData_LFP_fixed.edf')
     print()
 
+# from_mat_to_LFP_Hanna(subj='025')
 
-for x in ['018']:
-    from_mat_to_LFP_Hanna(subj=x)
+def from_mat_to_units(subj='025'):
+    subj_files_list = glob.glob(f'D:\\Hanna\\D{subj}\\spikesorting\\*')
+    ch_names = pd.read_csv(f'D:\\Hanna\\D{subj}\\montage.csv')
+    micro_chans = ch_names[ch_names['IS_MACRO'] == 0].reset_index()
+    for i, curr_file in enumerate(subj_files_list):
+        try:
+            f = sio.loadmat(curr_file)
+            data = f['cluster_class']
+            data = np.array(data).astype('int')
+            # index start from 0
+            chan_num = int(curr_file.split('CSC')[1].replace('.mat',''))
+            curr_name = micro_chans['LOCATION'][chan_num - 1].replace(' ', '')
+            for i in range(1, 4):
+                data[data[:, 0] == i][:, 1].tolist()
+                curr_data = data[data[:, 0] == i]
+                if curr_data.size > 0:
+                    curr_data = curr_data[:, 1]
+                    np.savetxt(f'D:\\Hanna\\D{subj}\\spikesorting\\{curr_name}_unit{i}.csv', curr_data, delimiter=',', fmt="%d")
+        except Exception as e:
+            pass
+
+
+# from_mat_to_units()